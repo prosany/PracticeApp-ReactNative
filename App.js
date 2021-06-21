52cd0fcbe579e89d256177e80ed5bdff09f8f7ab
Mahabub Sany <mahabubsany2@gmail.com>
2021-06-21 19:00:18 +0600

    [#1PracticeApp]

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [autoText, setAutoText] = useState('Open up App.js to start working on your app!')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{autoText}</Text>
+      <Button title="Change Text" onPress={() => setAutoText('Text Chnaged.')} />
       <StatusBar style="auto" />
     </View>
   );
